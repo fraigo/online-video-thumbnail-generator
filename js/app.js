150d501e226c3578e007dbaf8674959de224a70a
Francisco <franciscoigor@gmail.com>
2020-03-02 10:52:17 -0800

    Added video URL loading

--- js/app.js
+++ js/app.js
@@ -9,7 +9,14 @@ var w,h,ratio;
 //add loadedmetadata which will helps to identify video attributes
 
 video.addEventListener('loadedmetadata', function() {
+  console.log("Metadata loaded");
   videow.value = video.videoWidth;
+  if (video.objectURL){
+    URL.revokeObjectURL(video.src);
+  }
+  video.objectURL=false;
+  video.play();
+  video.pause();
   resize();
 },false);
 
@@ -30,16 +37,37 @@ function selectVideo(){
   file.click();
 }
 
-function loadVideo(){
+function loadVideoFile(){
   var fileInput = file.files[0];
   if (fileInput){
+      console.log("Loading...");
       console.log(fileInput);
+      /*
       var reader  = new FileReader();
+      reader.addEventListener("error", function () {
+        console.log("Error loading video data");
+      });
+      reader.addEventListener('progress',function(ev){
+        console.log("progress", ev.loaded, ev.total, Math.round(ev.loaded*100.0/ev.total));
+      });
       reader.addEventListener("load", function () {
+          console.log("Video data loaded");
+          video.preload="metadata";
           video.src = reader.result;
-      }, false);
+        }, false);
       reader.readAsDataURL(fileInput);
+      */
+      video.pleload="metadata";
+      video.objectURL=true;
+      video.src = URL.createObjectURL(fileInput);
       videow.disabled = false;
       snap.disabled = false;
   }
+}
+
+function loadVideoURL(url){
+  video.preload="metadata";
+  video.src = url;
+  videow.disabled = false;
+  snap.disabled = false;
 }
